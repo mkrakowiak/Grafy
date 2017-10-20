6233bbc819df0c42afbb57a39094b8b52f8c87ac
Michał Krakowiak <krakmis093@gmail.com>
2017-10-20 18:59:43 +0200

    dodawanie wierzchołka

--- Grafy/Program.cs
+++ Grafy/Program.cs
@@ -9,15 +9,47 @@ namespace Grafy
     class Grafy
     {
         int[,] Macierz;
+        int rozmiar=0;
+        public Grafy()
+        {
+            Macierz = new int[32, 32];
+            for (int q = 0; q < 32; q++)
+            {
+                for (int k = 0; k < 32; k++)
+                    Macierz[q, k] = -1;
 
+            }
+
+        }
+        public void dodajWierzcholek()
+        {
+            
+            int i=1;
+            
+       
+            Console.WriteLine("Dodać wierzchołek?");
+            i = Convert.ToInt32(Console.ReadLine());
+            while (i == 1)
+            {
+
+                for (int q = 0; q < 32; q++)
+                {
+                    Macierz[rozmiar, q] = 0;
+                    Macierz[q, rozmiar] = 0;
+
+                }
+                rozmiar++;
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+        }
 
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
-
+            Grafy graf = new Grafy();
+            graf.dodajWierzcholek();
 
         }
     }
