9d8b18306c1fac780a76144892020c0b040c94cc
Michał Krakowiak <krakmis093@gmail.com>
2017-10-31 18:57:54 +0100

    Jordan drzewa

--- Grafy/Program.cs
+++ Grafy/Program.cs
@@ -10,7 +10,7 @@ namespace Grafy
     class Grafy
     {
         int[,] Macierz;
-        int rozmiar=0;
+        int rozmiar = 0;
         public Grafy()
         {
             Macierz = new int[32, 32];
@@ -24,82 +24,83 @@ namespace Grafy
         }
         public void dodajWierzcholek()
         {
-                for (int q = 0; q < 32; q++)
-                {
-                    Macierz[rozmiar, q] = 0;
-                    Macierz[q, rozmiar] = 0;
+            for (int q = 0; q < 32; q++)
+            {
+                Macierz[rozmiar, q] = 0;
+                Macierz[q, rozmiar] = 0;
 
-                }
-                rozmiar++;
-                Console.WriteLine("Wierzchołek dodany. Jego numer to " + Convert.ToInt32(rozmiar - 1));             
-            
+            }
+            rozmiar++;
+            Console.WriteLine("Wierzchołek dodany. Jego numer to " + Convert.ToInt32(rozmiar - 1));
+
         }
         public void usunWierzcholek(int numerWiercholka)
-        {              
-                for (int q = 0; q < 32; q++)
-                {
-                    Macierz[numerWiercholka, q] = -1;
-                    Macierz[q, numerWiercholka] = -1;
-                }
-                Console.WriteLine("Wierzchołek usunięty");               
-            
+        {
+            for (int q = 0; q < 32; q++)
+            {
+                Macierz[numerWiercholka, q] = -1;
+                Macierz[q, numerWiercholka] = -1;
+            }
+            Console.WriteLine("Wierzchołek usunięty");
+
         }
         public void dodajKrawedz(int numerPierwszegoWierzcholka, int numerDrugiegoWierzcholka)
-        {              
-                if (Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] == 0)
-                {
-                    Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] = 1;
-                    Macierz[numerDrugiegoWierzcholka, numerPierwszegoWierzcholka] = 1;
-                }
-                else
-                    Console.WriteLine("Krawędź już istnieje");                   
+        {
+            if (Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] == 0)
+            {
+                Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] = 1;
+                Macierz[numerDrugiegoWierzcholka, numerPierwszegoWierzcholka] = 1;
+            }
+            else
+                Console.WriteLine("Krawędź już istnieje");
         }
 
         public void usunKrawedz(int numerPierwszegoWierzcholka, int numerDrugiegoWierzcholka)
         {
 
-                if (Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] == 1)
-                {
-                    Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] = 0;
-                    Macierz[numerDrugiegoWierzcholka, numerPierwszegoWierzcholka] = 0;
-                    Console.WriteLine("Krawędź usunięta");
-                }
-                else
-                    Console.WriteLine("Taka krawędź nie istnieje");                        
+            if (Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] == 1)
+            {
+                Macierz[numerPierwszegoWierzcholka, numerDrugiegoWierzcholka] = 0;
+                Macierz[numerDrugiegoWierzcholka, numerPierwszegoWierzcholka] = 0;
+                Console.WriteLine("Krawędź usunięta");
+            }
+            else
+                Console.WriteLine("Taka krawędź nie istnieje");
         }
-        public void policzStopien(int numerWierzcholka)
+        public int policzStopien(int numerWierzcholka)
         {
-            int stopien=0;                      
+            int stopien = 0;
             int i = 0;
             while (i < rozmiar)
             {
-                if (Macierz[numerWierzcholka, i] != -1 && Macierz[numerWierzcholka, i] ==1)
+                if (Macierz[numerWierzcholka, i] != -1 && Macierz[numerWierzcholka, i] == 1)
                 {
                     stopien++;
                 }
                 i++;
             }
-            Console.WriteLine("Stopień równy " + stopien);
+            return stopien;
+            // Console.WriteLine("Stopień równy " + stopien);
         }
 
 
         public void maxStopien()
         {
             int stopien = 0;
-            int numerWierzcholka = 0;           
+            int numerWierzcholka = 0;
             int i = 0;
-            int maxStopien=0;
+            int maxStopien = 0;
             while (numerWierzcholka < rozmiar)
             {
 
                 while (i < rozmiar)
                 {
-                    
+
                     if (Macierz[numerWierzcholka, i] != -1 && Macierz[numerWierzcholka, i] == 1)
                     {
                         stopien++;
                     }
-                   
+
                     i++;
                 }
                 if (maxStopien < stopien && Macierz[numerWierzcholka, i] != -1)
@@ -157,15 +158,15 @@ namespace Grafy
                 if (stopien % 2 == 0 && Macierz[numerWierzcholka, i] != -1)
                     parzyste++;
                 else
-                    if(stopien % 2 == 1 && Macierz[numerWierzcholka, i] != -1)
-                        nieparzyste++;
+                    if (stopien % 2 == 1 && Macierz[numerWierzcholka, i] != -1)
+                    nieparzyste++;
                 i = 0;
                 stopien = 0;
                 numerWierzcholka++;
             }
             Console.WriteLine("Liczba wierzchołków o parzystym stopniu = " + parzyste + " Liczba wierzchołków o nieparzystym stopniu = " + nieparzyste);
         }
-        public void ciagStopniWierzcholkow()
+        public ArrayList ciagStopniWierzcholkow()
         {
             int stopien = 0;
             int numerWierzcholka = 0;
@@ -184,29 +185,31 @@ namespace Grafy
                     }
                     i++;
                 }
-                if(Macierz[numerWierzcholka, i] != -1)
+                if (Macierz[numerWierzcholka, i] != -1)
                     ciagStopni.Add(stopien);
-               
+
                 i = 0;
                 stopien = 0;
                 numerWierzcholka++;
             }
             ciagStopni.Sort();
             ciagStopni.Reverse();
-            
-            Console.WriteLine("Ciąg stopni wierzchołków to: ");
+
+            //Console.WriteLine("Ciąg stopni wierzchołków to: ");
             foreach (int element in ciagStopni)
             {
-                Console.Write(element + " ");
+                //2Console.Write(element + " ");
             }
-           
+            return ciagStopni;
+
+
         }
 
         public void podgrafIzomorficzny()
         {
-           
+
             int[,] matrixIloczyn = new int[rozmiar, rozmiar];
-            bool jest =  false;
+            bool jest = false;
             for (int i = 0; i < rozmiar; i++)
             {
                 for (int j = 0; j < rozmiar; j++)
@@ -218,10 +221,10 @@ namespace Grafy
                         {
                             w += Macierz[i, k] * Macierz[k, j];
                         }
-                       
-                       
-                           
-                        
+
+
+
+
                     }
                     if (Macierz[i, j] == -1)
                         matrixIloczyn[i, j] = -1;
@@ -231,7 +234,7 @@ namespace Grafy
                     {
                         jest = true;
                     }
-                        
+
                 }
 
             }
@@ -262,15 +265,15 @@ namespace Grafy
             {
                 ciagStopni.Sort();
                 ciagStopni.Reverse();
-                while ((int)ciagStopni[0]>0)
+                while ((int)ciagStopni[0] > 0)
                 {
-                    
+
                     q = (int)ciagStopni[0];
                     ciagStopni.RemoveAt(0);
-                    
+
                     while (q != 0)
                     {
-                        ciagStopni[q-1] =(int) ciagStopni[q-1] - 1;
+                        ciagStopni[q - 1] = (int)ciagStopni[q - 1] - 1;
                         q--;
 
                     }
@@ -283,7 +286,7 @@ namespace Grafy
                 foreach (int element in ciagStopni)
                 {
                     if (element != 0)
-                        ok = false; 
+                        ok = false;
                 }
                 if (ok == false)
                     Console.WriteLine("Ciąg nie jest grafowy");
@@ -295,93 +298,132 @@ namespace Grafy
             else
                 Console.WriteLine("Ciąg liczb naturalnych nie jest ciągiem grafowym ");
         }
-    }
-
-
-
-    class Program
-    {
-        static void Main(string[] args)
+        public void jordan()
         {
-            int numerPierwszegoWierzchołka;
-            int numerDrugiegoWierzchołka;
-
-            int caseSwitch = 1;
-            Grafy graf = new Grafy();
+            ArrayList ciagStopni = new ArrayList();
             
-            while (caseSwitch !=0) {
-                Console.WriteLine();
-                Console.WriteLine("1-Dodaj wierzchołek");
-                Console.WriteLine("2-Usuń wierzchołek");
-                Console.WriteLine("3-Dodaj krawędź");
-                Console.WriteLine("4-Usuń krawędź");
-                Console.WriteLine("5-Stopień wierzchołka");
-                Console.WriteLine("6-Maksymalny stopień grafu");
-                Console.WriteLine("7-Minimalny stopień grafu");
-                Console.WriteLine("8-Wszaczenie ilości wierzchołków parzdystego i nie parzystego stopnia");
-                Console.WriteLine("9-Wypisanie Ciągu stopni wierzchołków");
-                Console.WriteLine("10-Sprawdź czy ciąg liczb naturalnych jest ciągiem grafowym");
-                Console.WriteLine("11-Sprawdź czy graf zawiera podgraf izomorwiczny do C3");
-                Console.WriteLine("0-Koniec");
-
-
-
-                caseSwitch = Convert.ToInt32(Console.ReadLine());
-                switch (caseSwitch)
+            int pomRozmiar = rozmiar;
+            
+            int q = 0;
+            
+            while (pomRozmiar>3)
+            {
+               
+                for (int i = 0; i < rozmiar; i++)
                 {
-                    case 0:
-                        break;
-                    case 1:
-                        graf.dodajWierzcholek();
-                        break;
-                    case 2:
-                        Console.WriteLine("Podaj wierzchołek do usunięcia");
-                        numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        graf.usunWierzcholek(numerPierwszegoWierzchołka);
-                        break;
-                    case 3:
-                        Console.WriteLine("Podaj pierwszy wierzchołek");
-                        numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Podaj drugi wierzchołek");
-                        numerDrugiegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        graf.dodajKrawedz(numerPierwszegoWierzchołka, numerDrugiegoWierzchołka);
-                        break;
-                    case 4:
-                        Console.WriteLine("Podaj pierwszy wierzchołek");
-                        numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Podaj drugi wierzchołek");
-                        numerDrugiegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        graf.usunKrawedz(numerPierwszegoWierzchołka, numerDrugiegoWierzchołka);
-                        break;
-                    case 5:
-                        Console.WriteLine("Podaj wierzchołek do obliczenia jego stopnia");
-                        numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
-                        graf.policzStopien(numerPierwszegoWierzchołka);
-                        break;
-                    case 6:
-                        graf.maxStopien();
-                        break;
-                    case 7:
-                        graf.minStopien();
-                        break;
-                    case 8:
-                        graf.parzystyNieparzystyStopien();
-                        break;
-                    case 9:
-                        graf.ciagStopniWierzcholkow();
-                        break;
-                    case 10:
-                        graf.sprCiagGrafowy();
-                        break;
-                    case 11:
-                        graf.podgrafIzomorficzny();
-                        break;
-                    default:
-                        Console.WriteLine("Zły wybór");
-                        break;
+                    if (pomRozmiar != 1)
+                                                                                      
+                    {
+
+                        if (this.policzStopien(i) == 1)
+                        {
+                            
+                            this.usunWierzcholek(i);
+                            pomRozmiar--;
+                        }
+                    }
                 }
+                
+                q++;
+            }
+           for(q=0; q < rozmiar; q++)
+            {
+                 if (Macierz[q, q] != -1)
+                Console.WriteLine("numer " + q);
             }
 
+        }
+
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int numerPierwszegoWierzchołka;
+                int numerDrugiegoWierzchołka;
+
+                int caseSwitch = 1;
+                Grafy graf = new Grafy();
+
+                while (caseSwitch != 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("1-Dodaj wierzchołek");
+                    Console.WriteLine("2-Usuń wierzchołek");
+                    Console.WriteLine("3-Dodaj krawędź");
+                    Console.WriteLine("4-Usuń krawędź");
+                    Console.WriteLine("5-Stopień wierzchołka");
+                    Console.WriteLine("6-Maksymalny stopień grafu");
+                    Console.WriteLine("7-Minimalny stopień grafu");
+                    Console.WriteLine("8-Wszaczenie ilości wierzchołków parzdystego i nie parzystego stopnia");
+                    Console.WriteLine("9-Wypisanie Ciągu stopni wierzchołków");
+                    Console.WriteLine("10-Sprawdź czy ciąg liczb naturalnych jest ciągiem grafowym");
+                    Console.WriteLine("11-Sprawdź czy graf zawiera podgraf izomorwiczny do C3");
+                    Console.WriteLine("0-Koniec");
+
+
+
+                    caseSwitch = Convert.ToInt32(Console.ReadLine());
+                    switch (caseSwitch)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            graf.dodajWierzcholek();
+                            break;
+                        case 2:
+                            Console.WriteLine("Podaj wierzchołek do usunięcia");
+                            numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            graf.usunWierzcholek(numerPierwszegoWierzchołka);
+                            break;
+                        case 3:
+                            Console.WriteLine("Podaj pierwszy wierzchołek");
+                            numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Podaj drugi wierzchołek");
+                            numerDrugiegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            graf.dodajKrawedz(numerPierwszegoWierzchołka, numerDrugiegoWierzchołka);
+                            break;
+                        case 4:
+                            Console.WriteLine("Podaj pierwszy wierzchołek");
+                            numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Podaj drugi wierzchołek");
+                            numerDrugiegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            graf.usunKrawedz(numerPierwszegoWierzchołka, numerDrugiegoWierzchołka);
+                            break;
+                        case 5:
+                            Console.WriteLine("Podaj wierzchołek do obliczenia jego stopnia");
+                            numerPierwszegoWierzchołka = Convert.ToInt32(Console.ReadLine());
+                            graf.policzStopien(numerPierwszegoWierzchołka);
+                            break;
+                        case 6:
+                            graf.maxStopien();
+                            break;
+                        case 7:
+                            graf.minStopien();
+                            break;
+                        case 8:
+                            graf.parzystyNieparzystyStopien();
+                            break;
+                        case 9:
+                            graf.ciagStopniWierzcholkow();
+                            break;
+                        case 10:
+                            graf.sprCiagGrafowy();
+                            break;
+                        case 11:
+                            graf.podgrafIzomorficzny();
+                            break;
+                        case 12:
+                            graf.jordan();
+                            break;
+                        default:
+                            Console.WriteLine("Zły wybór");
+                            break;
+                    }
+                }
+
+            }
         }
     }
 }
